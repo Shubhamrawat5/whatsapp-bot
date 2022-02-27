e3eb9f7c66631e84ac89a47129d2831651e71841
Shubhamrawat5 <sr856161@gmail.com>
2022-02-28 00:04:16 +0530

    fix insta downloader

--- functions/insta.js
+++ functions/insta.js
@@ -6,39 +6,53 @@ module.exports.getInstaVideo = async (url) => {
     videoDirectLink = "";
   try {
     if (url.includes("?")) url = url.slice(0, url.search("\\?"));
-    const res = await axios.get(url + "?__a=1", {
-      headers: {
-        accept:
-          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "accept-language": "en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7",
-        "cache-control": "max-age=0",
-        "sec-ch-ua":
-          '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
-        "sec-ch-ua-mobile": "?1",
-        "sec-fetch-dest": "document",
-        "sec-fetch-mode": "navigate",
-        "sec-fetch-site": "none",
-        "sec-fetch-user": "?1",
-        "upgrade-insecure-requests": "1",
-        cookie:
-          'ig_did=305179C0-CE28-4DCD-847A-2F28A98B7DBF; ig_nrcb=1; mid=YQBN3wAEAAGfSSDsZYS9nf2a5MHO; csrftoken=KItbBYAsObQgmJU2CsfqfiRFtk8JXwgm; sessionid=29386738134%3A8NwzjrA3jruVB4%3A23; ds_user_id=29386738134; fbm_124024574287414=base_domain=.instagram.com; shbid="18377\05429386738134\0541674226938:01f7d2db0f9c512fc79336716e1cf02623129a7897f5ccb8d878999be86c0e010bb77920"; shbts="1642690938\05429386738134\0541674226938:01f73e613a6030436ef5f2cea6c7402b82a96c1a61f905b746d3951f49a7f2d2eab6d399"; fbsr_124024574287414=Ps5NinG2AjNMV4W927e_vwMrZVLCltfcbWGS3B5S3to.eyJ1c2VyX2lkIjoiMTAwMDA5NDY1ODIwODQyIiwiY29kZSI6IkFRQlZrOVljMF9DS24tVEpqZ21VWjdPT2dOelFVdkJyLXUzaENSOGR0RzZrbVQxdWszYUMtVDZJeV9QWjBCc1lCcTBmZkxNZmsyUVlMM0hMVGVhQ1pxb1RRQzdsOE9BYlZKdmlvTU5GZ0dncVdxZVQzNV9JM3ZOV0pCR3BsWXVQX0dGMDJMMEt2aTk4WXpxNFhrVWhaVUNRanpPcUthN01aOVdZaVc5SVFzZjRxU3FQTXUzVXlwRWVsMXQ4TjJkV2ZHSnNFYXRsNXBIRXBGMlJSSWljY0F1c3BTZHNPdWFZSThCeV9uRFpjQklUUFk0RzNJY0NiYnFtdXNFZXY5ZUlsMVlZQ0E0bE5ROWxyeGtZdU1IM05scWRFTmtlQjNwWVRjRGlsZDZtekNpNFgzcnZIZUtUMFVFNkJFYVlURFpCTmhaOTd5TmJWT1R1ZENWdk84UlFoYjV2Iiwib2F1dGhfdG9rZW4iOiJFQUFCd3pMaXhuallCQU0zaHBjU2lKUm50WWcyTm0xamhlUlFkd3VCeExaQ1V0UjV5endGSkdVQVpDbERGRThwdXdaQXRPMkxtQnMxNjNiVGQzZERhRVl3UGRiWHY1bE5PNEZaQVVoYUpBZDBIcTQyWkN5OVdicXh4blVnZml5MHBETm9rMXlQVzlUNHpaQVVsbHVGcmZ4OFFhRlRnZG9wRTBFMDBMaGg3OVhuWkN1QldteWZ0MlpBY1NYVUpMRjNWNzUwWkQiLCJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImlzc3VlZF9hdCI6MTY0MjY5NDAyM30; rur="VLL\05429386738134\0541674231548:01f7816fe2a5156acdb86c5eff76c0ae83ac053646c44ccc592f854fb9d24a18bfcfc3ac"',
-      },
-      referrerPolicy: "strict-origin-when-cross-origin",
-      body: null,
-      method: "GET",
-      mode: "cors",
-    });
+    // const res = await axios.get(url + "?__a=1", {
+    //   headers: {
+    //     accept:
+    //       "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    //     "accept-encoding": "gzip, deflate, br",
+    //     "accept-language": "en-US,en;q=0.9",
+    //     "cache-control": "no-cache",
+    //     cookie:
+    //       "ig_did=305179C0-CE28-4DCD-847A-2F28A98B7DBF; ig_nrcb=1; mid=YQBN3wAEAAGfSSDsZYS9nf2a5MHO; fbm_124024574287414=base_domain=.instagram.com; csrftoken=4f5QNVgM7ioSEf7nfJFf3WJiZU7oR3Gz",
+    //     pragma: "no-cache",
+    //     "sec-ch-ua":
+    //       '" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
+    //     "sec-ch-ua-mobile": "?0",
+    //     "sec-ch-ua-platform": "Windows",
+    //     "sec-fetch-dest": "document",
+    //     "sec-fetch-mode": "navigate",
+    //     "sec-fetch-site": "none",
+    //     "sec-fetch-user": "?1",
+    //     "upgrade-insecure-requests": "1",
+    //     "user-agent":
+    //       "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
+    //   },
+    //   referrerPolicy: "strict-origin-when-cross-origin",
+    //   body: null,
+    //   method: "GET",
+    //   mode: "cors",
+    // });
     // console.log(res.data.items[0]);
+    const res = await axios.get(
+      `https://api.neoxr.eu.org/api/ig?url=${url}&apikey=yourkey`
+    );
+
+    if (res.data.data[0].type === "mp4") {
+      videoDirectLink = res.data.data[0].url;
+    } else if (res.data.data[0].type === "jpg") {
+      imgDirectLink = res.data.data[0].url;
+    }
 
     // if (res.status == 200 && res.data.items[0].video_versions) {
     //   videoDirectLink = res.data.items[0].video_versions[0].url;
     // }
     // imgDirectLink = res.data.items[0].image_versions2.candidates[0].url;
 
-    if (res.status == 200 && res.data.graphql.shortcode_media.is_video) {
-      videoDirectLink = res.data.graphql.shortcode_media.video_url;
-    }
-    imgDirectLink = res.data.graphql.shortcode_media.display_url;
+    // if (res.status == 200 && res.data.graphql.shortcode_media.is_video) {
+    //   videoDirectLink = res.data.graphql.shortcode_media.video_url;
+    // }
+    // imgDirectLink = res.data.graphql.shortcode_media.display_url;
   } catch (err) {
     console.log(err);
   }
@@ -48,3 +62,4 @@ module.exports.getInstaVideo = async (url) => {
 
 // getInstaVideo("https://www.instagram.com/p/CV7guhtM1R2/");
 // getInstaVideo("https://www.instagram.com/reel/CK-c9q-gD-M/");
+// getInstaVideo("https://www.instagram.com/reel/CaUqEQal22W/");
